7a565073fb83e8171eb33fd8cef5d326827d62c4
deviltt <tt416926063@qq.com>
2017-12-09 00:03:38 +0800

    vector<type> variable;variable.push_back();

---
+++ chapter3/3.14.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int num;
+	vector<int> vector1;
+
+	while(cin >> num)
+		vector1.push_back(num);
+
+	return 0;
+}
---
+++ chapter3/3.15.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	string str;
+	vector<string> vector1;
+
+	while(cin >> str)
+		vector1.push_back(str);
+
+	return 0;
+}
