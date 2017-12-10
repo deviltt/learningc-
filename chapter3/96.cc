4b7fe2384046772b8ec070039992fc272351f547
deviltt <tt416926063@qq.com>
2017-12-10 23:06:00 +0800

    isspace()

--- chapter3/96.cc
+++ chapter3/96.cc
@@ -5,10 +5,15 @@ using namespace std;
 int main()
 {
 	string s("hello world\n");
-
+/*
 	if(s.begin() != s.end()){
 		auto it = s.begin();
 		*it = toupper(*it);
 	}
+*/
+	for(auto it = s.begin(); it != s.end() && !isspace(*it); it++){
+		*it = toupper(*it);
+	}
+
 	cout << s;
 }
